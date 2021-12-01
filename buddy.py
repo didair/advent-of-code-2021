1b39ab8f1813f12a309ece86a275e942e3f46cda
Andreas Ekström <didair@msn.com>
2021-12-02 00:14:58 +0100

    remove class init from buddy and update file read method

--- buddy.py
+++ buddy.py
@@ -1,25 +1,22 @@
 # Advent of Code 2021
 # Author: Andreas Ekström <didair>
 
-import csv
 
 class Buddy:
 
-    def __init__(self, options):
-        self.options = options;
+    # Read data from file, and return the data as array
+    def readDataFromFile(self, filename):
+        with open(filename) as f:
+            output = [int(x) for x in f.readlines()];
 
-    # Read data from file, and return the data as array 
-    def readDataFromCSV(file):
-        with open(file) as f:
-            data = f.read().splitlines();
-            return data;
+        return output;
 
     # Prints each argument and wraps it in a christmas-y theme
-    def outputAnswer(*answers):
+    def outputAnswer(self, *answers):
         print("🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄");
 
         for answer in answers:
-            if answer is not None
+            if type(answer) == str:
                 print(answer);
             else:
                 pass;
