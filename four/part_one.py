9c9f5fd1749502d1cb5cf8b661fb340f60fd1719
Andreas Ekström <didair@msn.com>
2021-12-06 01:25:51 +0100

    Add solution for day four, part one

---
+++ four/part_one.py
@@ -0,0 +1,95 @@
+# Advent of Code 2021
+# Author: Andreas Ekström <didair>
+
+import sys
+sys.path.append("..");
+
+from buddy import Buddy
+
+def check_row(row, draw_numbers):
+    matches = 0;
+
+    for number in row:
+        if number in draw_numbers:
+            matches += 1;
+
+    if matches == len(row):
+        return True;
+    else:
+        return False;
+
+def check_column(board, draw_numbers):
+    for column in range(len(board[0])):
+        matches = 0;
+        for row in board:
+            if row[column] in draw_numbers:
+                matches += 1;
+
+        if matches == len(board[0]):
+            return True;
+
+    return False;
+
+def check_bingo(board, draw_numbers):
+    for row in board:
+        if check_row(row, draw_numbers):
+            return True;
+
+    return check_column(board, draw_numbers);
+
+def day_four():
+    draw_numbers = [];
+    raw_boards = [];
+    boards = [];
+
+    with open('input.in') as file:
+        for line in file.read().splitlines():
+            if ',' in line:
+                draw_numbers = line.split(',');
+            else:
+                raw_boards.append(line);
+
+    board = [];
+    for index, line in enumerate(list(raw_boards)):
+        if line == '' and board != []:
+            boards.append(board);
+            board = [];
+        else:
+            new_row = line.strip().replace('  ', ' ');
+
+            if new_row != '':
+                new_row = new_row.split(' ');
+                board.append(new_row);
+
+                if index == len(raw_boards) - 1:
+                    boards.append(board);
+
+    drawed_numbers = [];
+    winning_board = False;
+    unmarked_score = 0;
+
+    for number in draw_numbers:
+        drawed_numbers.append(number);
+
+        for index, board in enumerate(list(boards)):
+            if check_bingo(board, drawed_numbers):
+                winning_board = board;
+                break;
+        else:
+            continue;
+
+        break;
+
+    if winning_board is not False:
+        for row in winning_board:
+            for number in row:
+                if number not in drawed_numbers:
+                    unmarked_score += int(number);
+
+    Buddy().outputAnswer(
+        "Unmarked score: " + str(unmarked_score),
+        "Answer is: " + str(unmarked_score * int(drawed_numbers[-1])),
+    );
+
+## 19 days until christmas 🎄🎅🏻🎄🎅🏻🎄🎅🏻🎄🎅🏻🎄
+day_four();
